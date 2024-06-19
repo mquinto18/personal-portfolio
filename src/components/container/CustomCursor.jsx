bafb2200d0aef3e30f198773a057b5a8efd02bec
mquinto18 <quintom53@gmail.com>
2024-06-19 21:40:52 +0800

    new

--- src/components/container/CustomCursor.jsx
+++ src/components/container/CustomCursor.jsx
@@ -1,31 +1,38 @@
 import React, { useState, useEffect } from "react";
-
+import { FaArrowRightLong } from "react-icons/fa6";
 function CustomCursor() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const addListeners = () => {
-      document.addEventListener("mousemove", onMouseMove);
-    };
-
     const onMouseMove = (e) => {
       setPosition({ x: e.clientX, y: e.clientY });
     };
 
-    addListeners();
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
 
     return () => {
       document.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <div
-      className="custom-cursor"
-      style={{ left: `${position.x}px`, top: `${position.y}px` }}
-    >
-      {/* Your cursor design, e.g., an orange circle */}
-    </div>
+    !isMobile && (
+      <div
+        className="custom-cursor flex justify-center items-center text-white"
+        style={{ left: `${position.x}px`, top: `${position.y}px` }}
+      >
+        <FaArrowRightLong />
+      </div>
+    )
   );
 }
 
